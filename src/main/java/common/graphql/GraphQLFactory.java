c28ddbb0b0757dc59be3113b2fb677863117ad06
Lim, Chee Kin <chee-kin.lim@dxc.com>
2020-09-01 17:23:13 +0800

    refactored code using graphql-java-tools

--- src/main/java/common/graphql/GraphQLFactory.java
+++ src/main/java/common/graphql/GraphQLFactory.java
@@ -1,51 +1,31 @@
 package common.graphql;
 
-import java.util.Collection;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import graphql.GraphQL;
+import graphql.kickstart.tools.SchemaParser;
+import graphql.kickstart.tools.SchemaParserBuilder;
 import graphql.schema.GraphQLSchema;
-import io.leangen.graphql.GraphQLSchemaGenerator;
-import io.micronaut.context.BeanContext;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
-import io.micronaut.inject.qualifiers.Qualifiers;
-import graphql.GraphQL;
+
+import javax.inject.Singleton;
 
 @Factory
+@SuppressWarnings("Duplicates")
 public class GraphQLFactory {
-
-    public static final Logger logger = LoggerFactory.getLogger(GraphQLFactory.class);
-
-    @Inject
-    protected BeanContext beanContext;
 
     @Bean
     @Singleton
-    public GraphQL graphQL() {
-        GraphQLSchemaGenerator schemaGenerator = new GraphQLSchemaGenerator(); // 1
+    public GraphQL graphQL(PersonQueryResolver personQueryResolver) {
 
-        Collection graphQLServices = beanContext.getBeansOfType(Object.class,
-                Qualifiers.byStereotype(GraphQLService.class)); // 2
+        // Parse the schema.
+        SchemaParserBuilder builder = SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(personQueryResolver);
 
-        if (graphQLServices.isEmpty()) { // 3
-            logger.debug("No GraphQL services found, returning empty schema");
-            return GraphQL.newGraphQL(GraphQLSchema.newSchema().build()).build();
-        } else { // 4
-            for (Object graphQLService : graphQLServices) {
-                Class graphQLServiceClass = graphQLService.getClass();
-                if (graphQLServiceClass.getSimpleName().contains("$Intercepted"))
-                    graphQLServiceClass = graphQLServiceClass.getSuperclass(); // 5
+        // Create the executable schema.
+        GraphQLSchema graphQLSchema = builder.build().makeExecutableSchema();
 
-                logger.debug("Registering GraphQL service: {}", graphQLServiceClass.getSimpleName());
-                schemaGenerator.withOperationsFromSingleton(graphQLService, graphQLServiceClass); // 6
-            }
-        }
-
-        return GraphQL.newGraphQL(schemaGenerator.generate()).build();
+        // Return the GraphQL bean.
+        return GraphQL.newGraphQL(graphQLSchema).build();
     }
-
 }
