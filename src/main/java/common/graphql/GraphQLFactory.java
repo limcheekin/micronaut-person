5ecc2a12b2014edf7f75a848c687bc63e5c955f5
Lim, Chee Kin <chee-kin.lim@dxc.com>
2020-09-02 13:37:15 +0800

    first import of graphql-java implementation

--- src/main/java/common/graphql/GraphQLFactory.java
+++ src/main/java/common/graphql/GraphQLFactory.java
@@ -1,11 +1,18 @@
 package common.graphql;
 
 import graphql.GraphQL;
-import graphql.kickstart.tools.SchemaParser;
-import graphql.kickstart.tools.SchemaParserBuilder;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.core.io.ResourceResolver;
+
+import javax.inject.Singleton;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import javax.inject.Singleton;
 
@@ -15,15 +22,23 @@ public class GraphQLFactory {
 
     @Bean
     @Singleton
-    public GraphQL graphQL(PersonQueryResolver personQueryResolver) {
+    public GraphQL graphQL(ResourceResolver resourceResolver, PersonDataFetcher personDataFetcher) {
+        SchemaParser schemaParser = new SchemaParser();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
 
         // Parse the schema.
-        SchemaParserBuilder builder = SchemaParser.newParser()
-                .file("schema.graphqls")
-                .resolvers(personQueryResolver);
+        TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
+        typeRegistry.merge(schemaParser.parse(new BufferedReader(new InputStreamReader(
+                resourceResolver.getResourceAsStream("classpath:schema.graphqls").get()))));
+
+        // Create the runtime wiring.
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("findById", personDataFetcher))
+                .build();
 
         // Create the executable schema.
-        GraphQLSchema graphQLSchema = builder.build().makeExecutableSchema();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
 
         // Return the GraphQL bean.
         return GraphQL.newGraphQL(graphQLSchema).build();
