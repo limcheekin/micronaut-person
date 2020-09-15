9b8065df936aaf8da9cccfd9e4e6a24696bdc23e
Lim, Chee Kin <limcheekin@vobject.com>
2020-09-15 17:56:08 +0800

    update application config

--- src/main/java/common/graal/Target_org_mariadb_jdbc_internal_com_send_authentication_SendPamAuthPacket.java
+++
@@ -1,9 +0,0 @@
-package common.graal;
-
-import com.oracle.svm.core.annotate.Delete;
-import com.oracle.svm.core.annotate.TargetClass;
-
-@Delete
-@TargetClass(className = "org.mariadb.jdbc.internal.com.send.authentication.SendPamAuthPacket")
-final class Target_org_mariadb_jdbc_internal_com_send_authentication_SendPamAuthPacket {
-}
