22521ca07ca40df7885672e47c44ebea760c8b99
Lim, Chee Kin <limcheekin@vobject.com>
2020-09-24 10:16:49 +0800

    removed @Validated from controller and clean up imports

--- src/main/java/micronaut/person/PersonController.java
+++ src/main/java/micronaut/person/PersonController.java
@@ -4,17 +4,12 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.validation.Validated;
-
-import java.util.List;
 import java.util.Optional;
 
-import javax.annotation.Nullable;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @Controller("/people")
-@Validated
 public class PersonController {
     private final PersonService personService;
 
